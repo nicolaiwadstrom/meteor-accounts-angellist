ba5091fa19896d31ec9734003838b04e5b7f775f
Nicolai Wadstrom <nicolai.wadstrom@bootstraplabs.com>
2015-06-21 11:15:49 -0700

    First version.

---
+++ README.md
@@ -0,0 +1,14 @@
+Meteor Accounts AngelList
+=========================
+
+Allow users to connect via their AngelList account.  
+
+Inspired and based on gwendall:accounts-angellist
+
+
+Installation
+------------
+
+``` sh
+meteor add nicolaiwadstrom:meteor-accounts-angellist
+```
---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: "nicolaiwadstrom:meteor-accounts-angellist",
+  summary: "Login service for AngelList accounts",
+  version: "0.0.1",
+  git: "https://github.com/nicolaiwadstrom/meteor-accounts-angellist.git"
+});
+
+Package.onUse(function(api) {
+
+  api.use("accounts-base@1.2.0", ["client", "server"]);
+  api.imply("accounts-base", ["client", "server"]);
+  api.use("accounts-oauth@1.1.5", ["client", "server"]);
+
+  api.use("oauth@1.1.4", ["client", "server"]);
+  api.use("oauth2@1.1.3", ["client", "server"]);
+  api.use("http@1.1.0", ["server"]);
+  api.use("underscore@1.0.3", "server");
+  api.use("templating@1.1.1", "client");
+  api.use("random@1.0.3", "client");
+  api.use("service-configuration@1.0.4", ["client", "server"]);
+
+  api.addFiles("lib/accounts.js");
+  api.addFiles("lib/client.js", "client");
+  api.addFiles("lib/server.js", "server");
+
+});
