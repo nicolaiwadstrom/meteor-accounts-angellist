e39157bb8829d207d07c065865723cdf4817efc9
Nicolai Wadstrom <nicolai.wadstrom@bootstraplabs.com>
2015-07-02 15:08:10 -0700

    Updated platform version dependencies.

--- package.js
+++ package.js
@@ -1,21 +1,22 @@
 Package.describe({
   name: "nicolaiwadstrom:meteor-accounts-angellist",
   summary: "Login service for AngelList accounts, that works with the meteor standard accounts package.",
-  version: "0.0.15"
+  version: "0.0.15",
   git: "https://github.com/nicolaiwadstrom/meteor-accounts-angellist.git"
 });
 
 Package.onUse(function(api) {
-  api.use('underscore', ['server']);
-  api.use('accounts-base', ['client', 'server']);
-  // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('nicolaiwadstrom:meteor-angellist', ['client', 'server']);
+	api.versionsFrom('1.1.0.2');
+	api.use('underscore', ['server']);
+	api.use('accounts-base', ['client', 'server']);
+	// Export Accounts (etc) to packages using this one.
+	api.imply('accounts-base', ['client', 'server']);
+	api.use('accounts-oauth', ['client', 'server']);
+	api.use('nicolaiwadstrom:meteor-angellist@0.0.1', ['client', 'server']);
 
-  api.use('http', ['client', 'server']);
+	api.use('http', ['client', 'server']);
 
-  api.addFiles('angellist_login_button.css', 'client');
+	api.addFiles('angellist_login_button.css', 'client');
 
-  api.addFiles("angellist.js");
+	api.addFiles("angellist.js");
 });
